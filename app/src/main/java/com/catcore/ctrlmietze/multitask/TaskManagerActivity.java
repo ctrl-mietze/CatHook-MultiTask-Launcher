3f0a1b844785e8a0f63cc1b9311551f7f263f8f5
Ctrl Mietze <raphaelmietz@gmail.com>
2026-09-22 06:10:10 +0200

    Replace accidental literal line breaks

--- app/src/main/java/com/catcore/ctrlmietze/multitask/TaskManagerActivity.java
+++ app/src/main/java/com/catcore/ctrlmietze/multitask/TaskManagerActivity.java
@@ -72,13 +72,15 @@ public final class TaskManagerActivity extends AppCompatActivity {
         root.addView(modes, modeParams);
 
         RadioButton all = new RadioButton(this);
-        all.setId(View.generateViewId());\n        all.setText("View All");
+        all.setId(View.generateViewId());
+        all.setText("View All");
         all.setTextColor(Color.WHITE);
         all.setChecked(true);
         modes.addView(all, new RadioGroup.LayoutParams(0, -2, 1));
 
         RadioButton multi = new RadioButton(this);
-        multi.setId(View.generateViewId());\n        multi.setText("View MultiTask");
+        multi.setId(View.generateViewId());
+        multi.setText("View MultiTask");
         multi.setTextColor(Color.WHITE);
         modes.addView(multi, new RadioGroup.LayoutParams(0, -2, 1));
 
