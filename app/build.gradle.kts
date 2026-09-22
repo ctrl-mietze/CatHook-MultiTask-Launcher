64d614ffb7e1e4d792a2cafee3b1a7a235e588a1
Ctrl Mietze <raphaelmietz@gmail.com>
2026-09-22 11:30:15 +0000

    v2: bump major rework test build to dev3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,8 +8,8 @@ android {
         applicationId = "com.catcore.ctrlmietze.multitask"
         minSdk = 28
         targetSdk = 36
-        versionCode = 20001
-        versionName = "2.0.0.0-dev2"
+        versionCode = 20002
+        versionName = "2.0.0.0-dev3"
     }
 
     signingConfigs {
