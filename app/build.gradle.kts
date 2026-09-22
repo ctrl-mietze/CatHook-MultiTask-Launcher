44c3a5d8bf2343dc2ee70bc72789088038ebf887
Ctrl Mietze <raphaelmietz@gmail.com>
2026-09-22 20:33:46 +0000

    build: bump V2 development build to dev5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,8 +8,8 @@ android {
         applicationId = "com.catcore.ctrlmietze.multitask"
         minSdk = 28
         targetSdk = 36
-        versionCode = 20003
-        versionName = "2.0.0.0-dev4"
+        versionCode = 20004
+        versionName = "2.0.0.0-dev5"
     }
 
     signingConfigs {
