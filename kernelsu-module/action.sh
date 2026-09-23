6cb2e59b1640745355fe44cf53366a528801e135
Ctrl Mietze <raphaelmietz@gmail.com>
2026-09-24 00:46:46 +0200

    repo: initialize CatHook Termux package feed

--- kernelsu-module/action.sh
+++
@@ -1,9 +0,0 @@
-#!/system/bin/sh
-MODDIR=${0%/*}
-
-echo "CatCore MultiTask Root Helper"
-echo
-"$MODDIR/bin/catcore-helper" status
-echo
-echo "Any stale session priority state will now be restored."
-"$MODDIR/bin/catcore-helper" restore-all
--- kernelsu-module/uninstall.sh
+++
@@ -1,5 +0,0 @@
-#!/system/bin/sh
-MODDIR=${0%/*}
-
-"$MODDIR/bin/catcore-helper" restore-all >/dev/null 2>&1
-rm -rf /data/adb/catcore_multitask
